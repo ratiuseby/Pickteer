5121b7c39cdceb4ec62269fa91f321cea0d0a822
Ratiu Sebastian <ratiusebastian314@gmail.com>
2019-12-14 17:50:42 +0200

    Changes to the form creation tool

--- src/main/java/com/pickteer/config/SecurityConfig.java
+++ src/main/java/com/pickteer/config/SecurityConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.pickteer.service.UserService;
@@ -21,8 +23,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
     private UserService userService;
 	
+	@Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
+	
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
+		http.requiresChannel()
+        .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+        .requiresSecure()
+        .and()
+        .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        .and()
         .authorizeRequests()
         	.antMatchers(
         		"/",
